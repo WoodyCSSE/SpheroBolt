624661f4336e93e9d691fd432d878611828065fd
LandonHarter <landonharter@outlook.com>
2022-01-27 12:39:26 -0500

    Chicken noise checkpoint - Woody

--- Program/HauntedMansion.js
+++ Program/HauntedMansion.js
@@ -7,9 +7,18 @@ Program: Haunted Mansion Maze
 
 async function startProgram() {
     await toBlueLight();
+	await toMusic1();
 }
 
 async function toBlueLight() {
-    await roll(0, 100, 1.4);
+    await roll(0, 100, 1.45);
 	setMainLed({ r: 0, g: 0, b: 255 });
+	
+}
+
+async function toMusic1() {
+	await delay (1);
+	await roll((getHeading()+90),100,0.9);
+	await Sound.Animal.Chicken.play(true);
+	await delay (1);
 }
