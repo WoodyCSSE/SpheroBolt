4267d2100a712b990aaf3f59e91c8d4f4973497d
LandonHarter <landonharter@outlook.com>
2022-01-27 12:22:49 -0500

    To blue light

--- Program/HauntedMansion.js
+++ Program/HauntedMansion.js
@@ -6,5 +6,10 @@ Program: Haunted Mansion Maze
 */
 
 async function startProgram() {
-  
+    await toBlueLight();
+}
+
+async function toBlueLight() {
+    await roll(0, 100, 1.4);
+	setMainLed({ r: 0, g: 0, b: 255 });
 }
